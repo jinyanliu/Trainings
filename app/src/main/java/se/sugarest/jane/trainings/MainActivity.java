8d84143349c6ce326c60732799e9f2bfa12db016
JinyanLiu <jane.jinyan.liu@gmail.com>
2016-12-17 23:38:19 +0100

    Modify MainActivity java file to display the save icon in the menu.

--- app/src/main/java/se/sugarest/jane/trainings/MainActivity.java
+++ app/src/main/java/se/sugarest/jane/trainings/MainActivity.java
@@ -2,6 +2,8 @@ package se.sugarest.jane.trainings;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch(item.getItemId()){
+            // Respond to a click on the "Save" menu option
+            case R.id.action_save:
+                //
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
