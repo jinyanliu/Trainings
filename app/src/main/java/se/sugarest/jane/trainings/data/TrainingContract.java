c28bc54523138ce3c3a2813893b81dcf09aa01ae
JinyanLiu <jane.jinyan.liu@gmail.com>
2016-12-17 23:57:14 +0100

    Define TrainingContract java class under data package.

---
+++ app/src/main/java/se/sugarest/jane/trainings/data/TrainingContract.java
@@ -0,0 +1,34 @@
+package se.sugarest.jane.trainings.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by jane on 12/17/16.
+ */
+
+public class TrainingContract {
+
+    private TrainingContract() {
+    }
+
+    public static abstract class TrainingEntry implements BaseColumns {
+
+        public final static String TABLE_NAME = "trainings";
+
+        public final static String _ID = BaseColumns._ID;
+        public final static String COLUMN_TRAINING_DAY_OF_WEEK = "day of week";
+        public final static String COLUMN_TRAINING_TRAINING = "training";
+        public final static String COLUMN_TRAINING_TIME = "time";
+
+        public final static int DAY_OF_WEEK_MONDAY = 1;
+        public final static int DAY_OF_WEEK_TUESDAY = 2;
+        public final static int DAY_OF_WEEK_WEDNESDAY = 3;
+        public final static int DAY_OF_WEEK_THURSDAY = 4;
+        public final static int DAY_OF_WEEK_FRIDAY = 5;
+        public final static int DAY_OF_WEEK_SATURDAY = 6;
+        public final static int DAY_OF_WEEK_SUNDAY = 7;
+
+    }
+}
+
+
